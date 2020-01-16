29a6a1ffaec848657c52a87ad54b2e8f84eeee97
jeldridge <jeldridge@ucsd.edu>
2020-01-15 17:31:21 -0800

    Added HW02.

---
+++ Homeworks/Homework02/tests/q3_10.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 3_10',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(number_of_james, int)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
